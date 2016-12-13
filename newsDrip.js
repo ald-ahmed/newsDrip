51da90743d3f716c5bf1a0db1a97eb1ed67fa0f9
ald-ahmed <ald.ahmed@yahoo.com>
2016-12-12 19:26:07 -0800

    clean a news page, newsDrip

---
+++ newsDrip.js
@@ -0,0 +1,100 @@
+
+var script = document.createElement("script");
+  script.setAttribute("src", "http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js");
+  script.addEventListener('load', function() {
+    var script = document.createElement("script");
+    document.body.appendChild(script);
+  }, false);
+  document.body.appendChild(script);
+
+
+
+jQuery.fn.justtext = function() {
+
+    return $(this).clone()
+            .children()
+            .remove()
+            .end()
+            .text();
+
+};
+
+
+var max=0;
+var elementClass;
+
+
+$('img').remove();
+$('script').remove();
+$('noscript').remove();
+$('style').remove();
+$('nav').remove();
+$('table').remove();
+$('footer').remove();
+$('meta').remove();
+$('link').remove();
+$('textarea').remove();
+$('input').remove();
+$('iframe').remove();
+$('header').remove();
+
+$('*').each(function() {
+
+    $(this).removeAttr('alt');
+
+    if (($(this).justtext()).length>max && ($(this).justtext()).contains(".")) {
+
+
+    max = $(this).justtext().length;
+
+    console.log($(this).justtext());
+
+    if ($(this).attr('class')===null) {
+
+      var elementWithoutClass = $(this);
+
+      while (elementWithoutClass.attr('class') === null){
+        elementWithoutClass= elementWithoutClass.parent();
+      }
+
+      elementClass=elementWithoutClass.attr('class');
+      console.log("null - " + elementClass);
+
+    }
+    else {
+      elementClass=$(this).attr('class');
+      console.log("not null");
+
+    }
+
+
+  }
+
+});
+
+elementClass= "."+elementClass;
+console.log(elementClass);
+
+elementClass=elementClass.replace(/\s{2}/g," ").trim().replace(/\s/g,".");
+
+var content = $(elementClass).text();
+console.log(content);
+
+
+$('body').append("<div class='drip'></div>");
+
+$('.drip').append("<style>.drip{margin: 0 auto;position: relative;width: 40%;}<style>");
+
+$('.drip').append("<h1 class='title'>"+ $(document).prop('title')+"</h1>");
+
+$(elementClass).each(function() {
+  if ($(this).text().length===0){
+    return;
+  }
+  $('.drip').append("<p class='bodyText'>"+$(this).text()+"</p>");
+
+
+});
+
+
+$('div:not(.drip)').remove();
