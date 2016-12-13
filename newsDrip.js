862c09a124189fcdf41e66cde2fdf7c60c13d3b5
ald-ahmed <ald.ahmed@yahoo.com>
2016-12-12 19:40:23 -0800

    add a bookmarklet url and remove query from the main js file

--- newsDrip.js
+++ newsDrip.js
@@ -1,13 +1,4 @@
 
-var script = document.createElement("script");
-  script.setAttribute("src", "http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js");
-  script.addEventListener('load', function() {
-    var script = document.createElement("script");
-    document.body.appendChild(script);
-  }, false);
-  document.body.appendChild(script);
-
-
 
 jQuery.fn.justtext = function() {
 
