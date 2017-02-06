183638e79405b90420557551d713a196784d62c9
Ahmed Al Dulaimy <aldulaim@usc.edu>
2017-02-06 13:41:06 -0800

    splitting function into modules

--- newsDrip.js
+++ newsDrip.js
@@ -91,23 +91,4 @@ $('*').each(function() {
 });
 
 var content = $(elementClass).text();
-
-
-console.log("got this content " + content);
-
-
-$('body').append("<div class='drip'></div>");
-$('.drip').append("<style>.drip{margin: 0 auto;position: relative;width: 40%;}<style>");
-$('.drip').append("<h1 class='title'>"+ title+"</h1>");
-
-$(elementClass).each(function() {
-  if ($(this).text().length===0){
-    return;
-  }
-  $('.drip').append("<p class='bodyText'>"+$(this).text()+"</p>");
-
-
-});
-
-
-$('html:not(.drip)').remove();
+$.Storage.set("newsDripData", content);
---
+++ print.js
@@ -0,0 +1,15 @@
+var content = $.Storage.get("newsDripData");
+
+console.log("got this content " + content);
+
+
+$('body').append("<div class='drip'></div>");
+$('.drip').append("<style>.drip{margin: 0 auto;position: relative;width: 40%;}<style>");
+$('.drip').append("<h1 class='title'>"+ title+"</h1>");
+
+$(elementClass).each(function() {
+  if ($(this).text().length===0){
+    return;
+  }
+  $('.drip').append("<p class='bodyText'>"+$(this).text()+"</p>");
+});
